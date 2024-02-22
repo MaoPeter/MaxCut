af6b03ceb28f022203899e9675e6b94440487dd5
Peter Mao <pm498@cornell.edu>
2024-02-22 15:32:57 -0500

    add the weight matrix constructor (based on the matlab version)

---
+++ weight_matrix_constructor.py
@@ -0,0 +1,14 @@
+def makeWeightArray(name, size, parameters=[]):
+    W = []
+    match name:
+        case "Complete":
+            W = [[1 for j in range(size)] for i in range(size)]
+        case "Cycle":
+            W = [[1 if j == (i+1) % size or j == (i-1) % size else 0 for j in range(size)] for i in range(size)]
+        case "CompleteBipartite": #where parameters[0] is the largest vertex in the first partition
+                                  #and parameters[0]+1 is the smallest vertex in the second partition
+            W = [[1 if (j <= parameters[0] and i > parameters[0]) or (j > parameters[0] and i <= parameters[0])
+                  else 0 for j in range(size)] for i in range(size)]
+        case _:
+            return
+    return W
